05ff03355552d3638f53557f2aca4f411d409e9a
yangmila <1822403563@qq.com>
2021-04-01 16:32:03 +0800

    提交

---
+++ 2020_12/src/homework/MaxArea.java
@@ -0,0 +1,17 @@
+package homework;
+
+public class MaxArea {
+    public int maxArea(int[] height) {
+        int i = 0,j = height.length - 1,res = 0;
+        while(i < j){
+            if(height[i] < height[j]){
+                res = Math.max(res,(j - i) * height[i]);
+                i++;
+            }else{
+                res = Math.max(res,(j - i) * height[j]);
+                j--;
+            }
+        }
+        return res;
+    }
+}
